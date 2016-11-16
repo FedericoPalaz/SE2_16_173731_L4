d5049c16507420a0dc314fb62e6d41675c5f36bc
Palazzini Federico (ACC0085017) <federico.palazzini@sci-b106pc038x.unitn.it>
2016-11-16 12:07:34 +0100

    Aggiunti i file vuoti

---
+++ datamanager.js
@@ -0,0 +1,13 @@
+//download scripts
+//hide internal structure
+app.use('/script',express.static(__dirname+'/scripts/'));
+//in html
+<script src="script/script.js"></script>
+
+function Car(name,brand)
+{
+    this.name=name;
+    this.brand=brand;
+}
+
+var vars=[['1',new Car ("500","Fiat")],[],[]];
