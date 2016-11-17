e1ce273e99b9f016c3b441a8be934683b01a7ee0
FedericoPalaz <federico.palazzini@studenti.unitn.it>
2016-11-17 22:52:07 +0100

    Modificato i file:client,datamanager e script

--- datamanager.js
+++ datamanager.js
@@ -1,13 +1,78 @@
-//download scripts
-//hide internal structure
-app.use('/script',express.static(__dirname+'/scripts/'));
-//in html
-<script src="script/script.js"></script>
+//express lib
+var express = require('express');
+//inspect
+var util = require('util');
 
-function Car(name,brand)
+//instantiate express
+var app = express();
+
+//POST
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.set('port', (process.env.PORT || 1337));
+
+//use: for both POST and GET
+app.use('/', function(request, response) 
 {
-    this.name=name;
-    this.brand=brand;
-}
+    //set the headers of the responce
+    var headers = {};
+    //answer
+    headers["Content-Type"] = "application/json";
+    response.writeHead(200, headers);
 
-var vars=[['1',new Car ("500","Fiat")],[],[]];
+
+	var text = '';
+
+	if ( typeof request.body !== 'undefined' && request.body)
+	{
+        //the content of the POST receiced
+		text = "request.body: " + util.inspect(request.body) + "\n";
+		
+        //content of the post
+		var id,name,surname,level,salary;
+		
+		//if query is defined and not null
+		if ( typeof request.body.id !== 'undefined' && request.body.id)
+            
+			id = request.body.id;
+		else 
+			id = "not defined";
+		
+		if ( typeof request.body.name !== 'undefined' && request.body.name)
+            
+    		name = request.body.name;
+		else 
+			name = "not defined";
+		if ( typeof request.body.surname !== 'undefined' && request.body.surname)
+            
+			surname = request.body.surname;
+		else 
+			surname = "not defined";
+		
+		if ( typeof request.body.level !== 'undefined' && request.body.level)
+            
+    		level = request.body.level;
+		else 
+			level = "not defined";
+		if ( typeof request.body.salary !== 'undefined' && request.body.salary)
+            
+    		salary = request.body.salary;
+		else 
+			salary = "not defined";
+			    	
+        text = text + "post received: " + id+','+name+','+surname+','+level+','+salary;
+	}
+	else
+	{
+		text = "body undefined";
+	}
+	
+    response.end(text);
+
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+//download scripts
+//hide internal structure
+//app.use('/script',express.static(__dirname+'/scripts/'));
+//in html
+//<script src="script/script.js"></script>
+
+/*function Car(name,brand)
+{
+    this.name=name;
+    this.brand=brand;
+}
+
+var vars=[['1',new Car ("500","Fiat")],[],[]];
+*/
+
+//Fai comparire gli elementi nella form
+function Aggiungi_campi()
+{
+	document.getElementById("myform").style.visibility="visible";
+}
